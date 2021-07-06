bcf638456743aba30bf218493f0a36e51c34de59
ErCody <you@example.com>
2021-07-06 23:53:19 +0400

    Протестировал функции

--- TestSystem/main.cpp
+++ TestSystem/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include "FileSystem.h"
+#include "LoginSystem.h"
 int main() {
-	FileSystem::createBaseDirectory();
+	//FileSystem::createBaseDirectory();
+	std::string l = "Ergyun", p = "Ergyun2005";
+    std::cout << userReg(l,p);
+
 }
