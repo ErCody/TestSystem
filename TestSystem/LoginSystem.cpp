38bd217264443737251aeb675a1f5b0fc7d91872
ErCody <you@example.com>
2021-07-07 12:45:26 +0400

    Добавил методы passwordDecryption и passwordEncryption.

--- TestSystem/LoginSystem.cpp
+++ TestSystem/LoginSystem.cpp
@@ -1,9 +1,11 @@
 #include "LoginSystem.h"
 #include "FileSystem.h"
-#include <string>
-#include <algorithm>
+
 #include <fstream>
 #include <iostream>
+
+#include <string>
+
 
 //Локальная функция для проверки, существует ли такой пользователь или нет
 bool isUserExist(const std::string& userLogin) {
@@ -15,7 +17,7 @@ bool isUserExist(const std::string& userLogin) {
 	
 }
 
-//Локальная функция для проверки пароля по критериям. 
+//Локальная функция для проверки пароля по критериям
 bool passwordCheck(const std::string& userPassword) {
 	uint8_t upLetterCounter = 0;
 	uint8_t numCounter = 0;
@@ -41,7 +43,26 @@ bool passwordCheck(const std::string& userPassword) {
 	return true;
 }
 
-bool userLogin(const std::string& userLogin, const std::string& userPassword) {
+//Локальная функция для шифрования пароля
+void passwordEncryption(std::string& userPassword) {
+	for (size_t i = 0; i < userPassword.size() - 4; i++) {
+		userPassword[i] = userPassword[i] + i;
+	}
+	for (size_t i = 4; i < userPassword.size(); i++) {
+		userPassword[i] = userPassword[i] - i;
+	}
+}
+//Локальная функция для дешифровки пароля
+void passwordDecryption(std::string& userPassword) {
+	for (size_t i = 0; i < userPassword.size() - 4; i++) {
+		userPassword[i] = userPassword[i] - i;
+	}
+	for (size_t i = 4; i < userPassword.size(); i++) {
+		userPassword[i] = userPassword[i] + i;
+	}
+}
+
+bool userLogin(const std::string& userLogin, std::string& userPassword) {
 	if (FileSystem::isExist(userLogin)) {
 		/*
 		*  Code
@@ -51,13 +72,15 @@ bool userLogin(const std::string& userLogin, const std::string& userPassword) {
 	return true;
 }
 
-//user
-bool userReg(const std::string& userLogin, const std::string& userPassword) {
+bool userReg(const std::string& userLogin, std::string& userPassword) {
 	if (isUserExist(userLogin) || !passwordCheck(userPassword)) {
 		return false;
 	}
 	std::string path = USER_PATH + userLogin + ".txt";
 	std::ofstream newUser;
+
+	passwordEncryption(userPassword);
+
 	newUser.open(path);
 	newUser.write(reinterpret_cast<const char*>(userPassword.c_str()), userPassword.size());
 	return true;
--- TestSystem/LoginSystem.h
+++ TestSystem/LoginSystem.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <string>
 
-bool userLogin(const std::string& userLogin, const std::string& userPassword);
+bool userLogin(const std::string& userLogin, std::string& userPassword);
 
-bool userReg(const std::string& userLogin, const std::string& userPassword);
+bool userReg(const std::string& userLogin, std::string& userPassword);
