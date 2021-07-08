2942f2f43b1eb119b75f58c548b0fb4c81aac335
ErCody <you@example.com>
2021-07-08 23:30:23 +0400

    Добавил методы, переделал старые

--- TestSystem/FileSystem.cpp
+++ TestSystem/FileSystem.cpp
@@ -13,12 +13,32 @@ namespace FileSystem {
 		}
 		return false;
 	}
+
 	void createBaseDirectory() {
 		std::filesystem::create_directories("./files/userData/adminData");
 		std::filesystem::create_directories("./files/testData");
 	}
-	void createUserDirectory(const std::string& userLogin) {
-		std::string path = USER_PATH + userLogin;
+
+	void createDirectory(const std::string& path) {
 		std::filesystem::create_directory(path);
 	}
+
+	void createFile_w(const std::string& path, std::string text) {
+		std::ofstream newFile;
+		text += '\n';
+		newFile.open(path);
+		newFile.write(text.c_str(), text.size());
+	}
+
+	std::string getFileData(const std::string& path, line line) {
+		std::string data;
+		std::ifstream file;
+		file.open(path);
+		for (size_t i = 0; i < line; i++) {
+			std::getline(file, data);
+		}
+		return data;
+	}
+
+   
 }
--- TestSystem/FileSystem.h
+++ TestSystem/FileSystem.h
@@ -2,14 +2,30 @@
 #include <string>
 
 //Folder with user data
-#define USER_PATH "./files/userData/"
+const std::string USER_PATH = "./files/userData/";
+const std::string ADMIN_PATH = "./files/userData/adminData/";
+
+enum line {
+	PASSWORD = 1,
+	NAME,
+	NUMBER,
+	EMAIL,
+	PASPORT_NUM,
+	NONE = 0
+};
 
 namespace FileSystem {
 	bool isExist(const std::string& path);
 
 	void createBaseDirectory();
 
-	void createUserDirectory(const std::string& userLogin);
+	void createDirectory(const std::string& path);
+
+	void createFile_w(const std::string& path, std::string text);
+
+	std::string getFileData(const std::string& path, line line);
+
+
 }
 
 
